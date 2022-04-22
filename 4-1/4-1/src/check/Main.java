ada36ab4add086ac35dd2ac473fb673db5d5c2e5
shidakouki <103645882+shidakouki@users.noreply.github.com>
2022-04-22 23:14:11 +0900

    課題提出テスト
    4-1

--- 4-1/4-1/src/check/Main.java
+++ 4-1/4-1/src/check/Main.java
@@ -1,26 +1,28 @@
 package check;
 
+import constants.Constants;
+
 public class Main {
 
 	public static void main(String[] args) {
 	    
 	    Main printName = new Main();
-	    System.out.println("printNameメソッド → " + printName);
-	    
-	    Pet pet = new Pet(constants.CHECK_CLASS_JAVA, constants.CHECK_CLASS_HOGE);
+	    System.out.println(printName);
+	    	    
+	    Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 	    System.out.println(pet);
 	    
-	    RobotPet robotPet = new RobotPet(CHECK_CLASS_R2D2, CHECK_CLASS_LUKE);
+	    RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 	    System.out.println(robotPet);
 
 	}
 	
 	private String firstName = "shida";
 	private String lastName = "kouki";
-	
+	    
 	private void printName(String firstName,String lastName) {
 		String fullName = firstName + lastName;
-		System.out.println(fullName);
+		System.out.println("printNameメソッド → " + fullName);
 	}
 
 }
