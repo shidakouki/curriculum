aad223d373c5d385425507aeaa5c883fde8f861a
shidakouki <103645882+shidakouki@users.noreply.github.com>
2022-04-28 22:29:01 +0900

    課題提出6-1　修正後
    問題解決できませんでした。

--- 6-1/6-1/src/main/java/SelectServlet/SelectServlet.java
+++ 6-1/6-1/src/main/java/SelectServlet/SelectServlet.java
@@ -19,10 +19,10 @@ public class SelectServlet extends HttpServlet {
             throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         // 問① select.htmlから渡された値を受け取るために下記を修正しましょう。
-        String value = request.getParameter("args");
+        String value = request.getParameter("month");
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
         // 問② エビデンスと同じ表示になるように修正しましょう。
-        out.println(value);
+        out.println("今は"+ value + "です。");
     }
 }
