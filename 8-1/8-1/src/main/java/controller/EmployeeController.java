1ff0029db5e4375554cfebcd23c223e1010c3ecf
shidakouki <103645882+shidakouki@users.noreply.github.com>
2022-05-30 14:09:54 +0900

    課題提出8-1修正

--- 8-1/8-1/src/main/java/controller/EmployeeController.java
+++ 8-1/8-1/src/main/java/controller/EmployeeController.java
@@ -1,8 +1,8 @@
 package controller;
 
 /**
-* 社員情報管理コントローラー
-*/
+ * 社員情報管理コントローラー
+ */
 
 import java.io.IOException;
 
@@ -16,36 +16,36 @@ import jakarta.servlet.http.HttpServletResponse;
 import service.EmployeeService;
 
 public class EmployeeController extends HttpServlet {
-public void doPost(HttpServletRequest request, HttpServletResponse response)
-throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-try {
- // 問① index.htmlから送信されたIDとPassWordの値を取得できるように修正しましょう。
-String id = request.getParameter("id");
-String password = request.getParameter("pass");
+		try {
+			// 問① index.htmlから送信されたIDとPassWordの値を取得できるように修正しましょう。
+			String id = request.getParameter("id");
+			String password = request.getParameter("pass");
 
-/*
-* IDとPassWordと元に、社員情報を検索する関数の呼び出し、結果をJSPに渡す処理
-* ※ EmployeeBeanとEmployeeServiceをimportするのを忘れないでください。
-*/
+			/*
+			 * IDとPassWordと元に、社員情報を検索する関数の呼び出し、結果をJSPに渡す処理
+			 * ※ EmployeeBeanとEmployeeServiceをimportするのを忘れないでください。
+			 */
 
- // 問② EmployeeServiceクラスをインスタンス化する。
+			// 問② EmployeeServiceクラスをインスタンス化する。
 
- EmployeeService employeeService = new EmployeeService();
+			EmployeeService employeeService = new EmployeeService();
 
- // 問③ EmployeeBeanに、EmployeeServiceよりsearch関数を呼び出し、返り値を格納する。
- EmployeeBean employeeBean = new EmployeeBean();
- employeeBean = employeeService.search(id,password);
-  
- // 問④ nullの部分に適切な引数をセットする。
-request.setAttribute("EmployeeBean", employeeBean);
+			// 問③ EmployeeBeanに、EmployeeServiceよりsearch関数を呼び出し、返り値を格納する。
+			EmployeeBean employeeBean = new EmployeeBean();
+			employeeBean = employeeService.search(id,password);
 
-} catch (Exception e) {
-e.printStackTrace();
-} finally {
-ServletContext context = this.getServletContext();
-RequestDispatcher dispatcher = context.getRequestDispatcher("/index.jsp");
-dispatcher.forward(request, response);
-}
-}
+			// 問④ nullの部分に適切な引数をセットする。
+			request.setAttribute("EmployeeBean", employeeBean);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ServletContext context = this.getServletContext();
+			RequestDispatcher dispatcher = context.getRequestDispatcher("/index.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
 }
